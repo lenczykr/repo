f585a85657631aa8f1dc7e213e887ce7e7848254
lenczykr <lenczyk.r@gmail.com>
2017-04-07 22:05:28 +0200

    Add files via upload
    pierwsze zadanie

--- scrips.js
+++ scrips.js
@@ -1,10 +1,35 @@
+(function($) {
+
+$(document).ready(function() {
+	// alert("dupa");
+
+// $(".link1").on("click", function(){
+// 	// $(this).preventDefault();
+// 	alert("dupa");
+// });
+
+var test = $("a");
+var divki = $(".grid, .grid12");              //git
+var dupa = $(".nav a[href*='http']");                                    //gitara
+var dupek = $('input:radio:not(:checked)');
+         //   $('input:radio:not(:checked), input:checkbox:not(:checked)');
+var mam = $("div#text p:first:empty");
+var pag = $(".pagination-item:not(span)")
+
+  
+
+console.log(pag);
+
+
+
+//dupa.hljs(dupa);
+
+
+});
 
 
 
 
 
-function wysunie() {
-document.getElementById("wdol").classList.toggle("show");
 
-
-}
+})(jQuery);
